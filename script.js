158a5d63845dd443995eecffa7bf18c2fb62975b
Ashutosh Sunil Gore <130917119+Ashugore48@users.noreply.github.com>
2023-06-22 10:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.addEventListener('DOMContentLoaded', function() {
+  var selectElement = document.getElementById('colorSelect');
+  var removeButton = document.querySelector('input[type="button"]');
+  removeButton.addEventListener('click', function() {
+    var selectedOption = selectElement.options[selectElement.selectedIndex];
+    if (selectedOption) {
+      selectElement.removeChild(selectedOption);
+    }
+  });
+});
